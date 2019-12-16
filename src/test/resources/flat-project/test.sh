74d19fa6a31d95b098fbcd200affd5d95c598a65
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-12-16 20:34:45 +0100

    [#1-einfache-loesung] project fixed
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

---
+++ src/test/resources/flat-project/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf ~/.m2/repository/gui4j/
+echo ##########################
+../../../../mvnw -U clean jar-install:install
+#../../../../mvnw -U clean jar-install:install install
--- src/test/resources/unit/flat-project/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf ~/.m2/repository/gui4j/
-echo ##########################
-../../../../../mvnw -U clean jar-install:install
-../../../../../mvnw -U clean jar-install:install install
