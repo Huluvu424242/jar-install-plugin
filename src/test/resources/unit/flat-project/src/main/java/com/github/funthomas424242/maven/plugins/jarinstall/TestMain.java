fd08c0c53c27fe418ce7dfb7a4c3fcaf146d2544
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-12-16 19:51:40 +0100

    [#1_lifecycle] test project fixed
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

---
+++ src/test/resources/unit/flat-project/src/main/java/com/github/funthomas424242/maven/plugins/jarinstall/TestMain.java
@@ -0,0 +1,11 @@
+package com.github.funthomas424242.maven.plugins.jarinstall;
+
+import com.gui4j.*;
+
+
+public class TestMain {
+
+    public static void main(String[] args){
+        Gui4j gui4j = null;
+    }
+}
