539364a06e501a3c1ab75b8162c96581e9abedc5
FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>
2019-12-16 21:08:26 +0100

    [funzt] test project fixed
    Signed-off-by: FunThomas424242 (Thomas Schubert) <funthomas424242@gmail.com>

--- src/test/resources/flat-project/test.sh
+++ src/test/resources/flat-project/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf ~/.m2/repository/gui4j/
 echo ##########################
-../../../../mvnw -U clean jar-install:install
-#../../../../mvnw -U clean jar-install:install install
+../../../../mvnw -U clean jar-install:installjars
+../../../../mvnw -U clean jar-install:installjars install
